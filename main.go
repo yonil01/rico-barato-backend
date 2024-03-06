f5ff0850d28b9500899a3323476688211a79de05
76797001 <76797001@pronabec.edu.pe>
2024-03-06 01:04:55 -0500

    post static

--- main.go
+++ main.go
@@ -3,18 +3,15 @@ package main
 import (
 	"backend-comee/api"
 	"backend-comee/internal/env"
-	"fmt"
-	"os"
-	"strconv"
 )
 
 func main() {
 	c := env.NewConfiguration()
-	port := os.Getenv("PORT")
+	/*port := os.Getenv("PORT")
 
 	puerto, err := strconv.Atoi(port)
 	if err != nil {
 		fmt.Println("Error fatal in asignature port")
-	}
-	api.Start(puerto, c.App.ServiceName, c.App.LoggerHttp, c.App.AllowedDomains)
+	}*/
+	api.Start(c.App.Port, c.App.ServiceName, c.App.LoggerHttp, c.App.AllowedDomains)
 }
